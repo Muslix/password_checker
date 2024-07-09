24ae0f435f0b0aa186dcbe05a1799380a037a994
root <wolfer_martin@yahoo.de>
2024-07-09 15:34:14 +0200

    ....

---
+++ frontend1/src/App.vue
@@ -0,0 +1,18 @@
+<!-- src/App.vue -->
+
+<template>
+  <div id="app">
+    <PasswordChecker />
+  </div>
+</template>
+
+<script>
+import PasswordChecker from './components/PasswordChecker.vue';
+
+export default {
+  name: 'App',
+  components: {
+    PasswordChecker,
+  },
+};
+</script>
